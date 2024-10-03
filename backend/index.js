50eb4a3032e3bc8fcc334c8c50e9ac2cbb6a2c73
Jacob Berry <jacobberry777@yahoo.com>
2024-10-02 23:00:36 -0700

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express"
+import { mongoose } from "mongoose"
+//import User from "./model/user.js"
+//import userRoute from "./routes/user.route.js"
+
+const app = express()
+
+
+app.use(express.json())
+//app.use("/user", userRoute) //creating a route for user related functions
+
+
+
+const mongoConnect = async () => {
+    await mongoose.connect("mongodb://localhost:27017")
+    console.log('DB connected')
+}
+
+mongoConnect()
+
+app.listen(3000, () => console.log('server started on port 3000'))
