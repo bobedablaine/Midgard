a9024a67714e7be48959787760e0f75d08fc5811
Jacob Berry <jacobberry777@yahoo.com>
2024-10-02 23:06:56 -0700

    initial commit

---
+++ frontend/src/app.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+//import './App.css';
+
+function App() {
+    
+    return (
+        <div className="App">
+
+            <div className='container' id='tasks-container'>
+                <span id='header'>Testing</span>
+                {/* Populate with tasks from backend here */}
+            </div>
+        </div>
+    );
+}
+
+export default App;
