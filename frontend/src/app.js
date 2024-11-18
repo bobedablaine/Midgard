97f2ed95235bf1515a464df0c47357f3f3ee5f96
bobedablaine <jacobberry777@yahoo.com>
2024-11-18 12:11:47 -0800

    Creation of frontend pages, orginization, and navbar

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,35 +1,22 @@
 import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
 import { Routes, Route } from "react-router-dom"
 import LoginPage from "./views/Login/LoginPage.js";
 import ContentPage from "./views/Content/ContentPage.js";
 import RegisterPage from './views/Register/RegisterPage.js';
+import HomePage from './views/Home/HomePage.js';
+import SettingsPage from './views/Settings/SettingsPage.js';
 
-// function App() {
-    
-//     return (
-//         <>
-//             {isLoggedIn ? (
-//                 <ContentPage />
-//             ) : (
-//                 <LoginPage onLogin={handleLogin} />
-//             )}
-//         </>
-//         // <BrowserRouter>
-//         //     <Routes>
-//         //         <Route path="/Login" element={<LoginPage />} />
-//         //         <Route path="/Content" element={<ContentPage />} />
-//         //     </Routes>
-//         // </BrowserRouter>
-//     );
-// }
 const App = () => {
+
+    //still lacking logic to stop from navigating straigh to content through search bar without being logged in
     return (
         <>
             <Routes>
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/content" element={<ContentPage />} />
                 <Route path="/register" element={<RegisterPage />} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/settings" element={<SettingsPage />} />
             </Routes>
         </>
     );
