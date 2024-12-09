8995220c110c6725bcc27f55a1ed272dbbb51adc
bobedablaine <jacobberry777@yahoo.com>
2024-12-08 18:59:40 -0800

    Home and Settings pages

--- frontend/src/views/Home/HomePage.js
+++ frontend/src/views/Home/HomePage.js
@@ -1,25 +1,41 @@
 import NavBar from "../../components/NavBar.js";
+import { NavLink } from "react-router-dom";
 
 const HomePage = () => {
 
     return (
         <>
             <NavBar />
-            <div className="container">
-                <main className="main-content">
-                    <h1>Home</h1>
-                </main>
-                <section className="right-panel">
-                    <div className="tools">
-                        <button>Test me</button>
-                        <button>Practical exercise</button>
-                        <button>Further reading</button>
-                    </div>
-                    <div className="chat-box">
-                        <div className="chat-message">Hi, any questions for me?</div>
-                        <input type="text" placeholder="Type your message..." />
-                    </div>
-                </section>
+            <div className="logo-wrap">
+                <div className="tint"></div>
+                <video autoPlay muted loop id="backgroundHomeVideo">
+                    <source
+                        src="https://res.cloudinary.com/hcv8opupx/video/upload/v1723243584/AdobeStock_570132261_s39xqq.mp4"
+                        type="video/mp4"
+                    />
+                    Your browser does not support the video tag.
+                </video>
+                <img src="/image_2024-10-06_115327155-removebg-preview.png" className="homelogo" alt="Logo" />
+            </div>
+            <div className="container-Boxes">
+                <div className="content-Box">
+                    <NavLink to="/content">
+                        <img src="/cyberBook2.jpg" className="homePicture" alt="Textbook" />
+                    </NavLink>
+                    <h1>Learn</h1>
+                </div>
+                <div className="content-Box">
+                    <NavLink to="/content">
+                        <img src="/cyberTools2.jpg" className="homePicture" alt="Textbook" />
+                    </NavLink>
+                    <h1>Tools</h1>
+                </div>
+                <div className="content-Box">
+                    <NavLink to="/settings">
+                        <img src="/cyberGear2.jpg" className="homePicture" alt="Textbook" />
+                    </NavLink>
+                    <h1>Settings</h1>
+                </div>
             </div>
         </>
         
